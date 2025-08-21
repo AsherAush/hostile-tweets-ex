ef3b0b6259f19033020cd6cf8c97924a56b6e240
asher zeev aush <A0583251203@gmail.com>
2025-08-21 11:32:31 +0300

    start conect mongo

--- project/app/fetcher.py
+++ project/app/fetcher.py
@@ -1,19 +1,23 @@
 import os
 from pymongo import MongoClient
 
-
-class MongoFetcher:
+class Connection:
     def __init__(self):
-        # Load environment variables for MongoDB connection
-        user = os.getenv("MONGO_USER")
-        password = os.getenv("MONGO_PASS")
-        dbname = os.getenv("MONGO_DBNAME")
-        host = os.getenv("MONGO_HOST")
+        self.user = os.getenv("USERNAME","IRGC")
+        self.password = os.getenv("PASSWORD","iraniraniran")
+        self.db_name = os.getenv("DB_NAME","IranMalDB")
+        self.col_name = os.getenv("COLLECTION_NAME","tweets")
+        self.mongo_url = os.getenv("MONGO_URL","mongodb+srv://IRGC:iraniraniran@iranmaldb.gurutam.mongodb.net/")
+        self.client = MongoClient(self.mongo_url)
+        self.db = self.client[self.db_name]
+        self.collection = self.db[self.col_name]
 
-        uri = f"mongodb+srv://{user}:{password}@{host}/{dbname}?retryWrites=true&w=majority"
-        self.client = MongoClient(uri)
-        self.db = self.client[dbname]
-        self.collection = self.db["tweets"]  # שם הקולקשן שלך, תעדכן אם שונה
+    def get_all_data(self):
+        return list(self.collection.find({}))
 
-    def fetch_all(self):
-        return list(self.collection.find())
+
+if __name__ == "__main__":
+    conn = Connection()
+    data = conn.get_all_data()
+    print(f"Found {len(data)} documents")
+    print(data[:5])
